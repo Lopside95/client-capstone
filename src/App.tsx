684fd58e26103409c36f8b317e31322f9cf7458a
James Wallington <jameswallington11@gmail.com>
2024-11-25 19:28:00 +0000

    add folders, partials and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.scss";
+import Posts from "./pages/posts/Posts";
+import Post from "./pages/post/Post";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+        <Route path="/" element={<Posts />} />
+        <Route path="/posts/:id" element={<Post />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
