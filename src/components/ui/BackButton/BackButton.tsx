ff32319f0e68c0d8edb592fa4bd4724d9b213234
James Wallington <jameswallington11@gmail.com>
2025-01-23 11:04:52 +0000

    add dynamic home button

--- src/components/ui/BackButton/BackButton.tsx
+++ src/components/ui/BackButton/BackButton.tsx
@@ -1,18 +1,31 @@
-import { ArrowLeftIcon } from "evergreen-ui";
+import { ArrowLeftIcon, HomeIcon } from "evergreen-ui";
 import { useNavigate } from "react-router";
+import "./BackButton.scss";
 
 const BackButton = () => {
   const navigate = useNavigate();
 
   return (
-    <ArrowLeftIcon
-      position="absolute"
-      size={28}
-      top={15}
-      left={15}
-      cursor="pointer"
-      onClick={() => navigate(-1)}
-    />
+    <>
+      <ArrowLeftIcon
+        className="back-button"
+        position="absolute"
+        size={28}
+        top={15}
+        left={15}
+        cursor="pointer"
+        onClick={() => navigate(-1)}
+      />
+      <HomeIcon
+        className="home-button"
+        position="absolute"
+        size={28}
+        top={15}
+        left={15}
+        cursor="pointer"
+        onClick={() => navigate("/")}
+      />
+    </>
   );
 };
 
